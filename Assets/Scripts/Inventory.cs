34d8a106fc0aa4ca3ff3c2f9a9293ba428136a24
Brandon Service <brservice@comcast.net>
2020-11-11 16:09:38 -0500

    Adding inventory and pick up script

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private Inventory[] inventories;
+    private List<string> inventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Following code prevents more than one inventory from being created
+        inventories = FindObjectsOfType<Inventory>();
+        if (inventories.Length > 1)
+        {
+            Destroy(inventories[1]);
+            Destroy(inventories[1].gameObject);
+        }
+
+        //Allows the inventory to be accessed in other scenes
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddItem(string item)
+    {
+        inventory.Add(item);
+    }
+}
---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public string name;
+    private Inventory inventory;
+
+    Vector2 cursorPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = FindObjectOfType<Inventory>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Method to check for click
+    public void CheckForClick()
+    {
+        //Grab vector2 for cursor to use in AABB math
+        cursorPosition = Input.mousePosition;
+        cursorPosition = Camera.main.ScreenToWorldPoint(cursorPosition);
+
+        //Selection for objects
+        if (Input.GetMouseButtonDown(0))
+        {
+            //AABB collision test for cursor
+            if (cursorPosition.x < this.GetComponent<BoxCollider2D>().bounds.max.x && cursorPosition.x > this.GetComponent<BoxCollider2D>().bounds.min.x)
+            {
+                //Potential collision!
+                //Check the next condition in a nested if statement, just to not have a ton of &'s and to be more efficient
+                if (cursorPosition.y > this.GetComponent<BoxCollider2D>().bounds.min.y && cursorPosition.y < this.GetComponent<BoxCollider2D>().bounds.max.y)
+                {
+                    //Collision!
+                    //Add to inventory
+                    inventory.AddItem(name);
+                }
+            }
+        }
+    }
+}
