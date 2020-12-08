004d5c0de33192c87296a467df9efcd4cd4db99f
alessandro <anf9790@g.rit.edu>
2020-12-07 22:43:25 -0500

    Cleanup
    Fixing things based on my updates!

---
+++ Assets/Scripts/MenuMouse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Changes the mouse for scenes not containing actual gameplay.
+/// </summary>
+public class MenuMouse : MonoBehaviour
+{
+    public Texture2D mouseTexture;
+    public CursorMode cursorMode = CursorMode.ForceSoftware;
+    public Vector2 hotSpot = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(mouseTexture, hotSpot, cursorMode);
+    }
+}
